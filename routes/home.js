0786ca4a2db4f2f58e8135bc7d4f328c2d6354ce
Erin Thigpen <35975360+thigabiga@users.noreply.github.com>
2018-04-12 22:10:45 -0500

    putting contents all on one page

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,36 @@
+
+
+
+
 const router = require("express").Router();
 
+let projectJSON = require("../data/projects.json");
+
 router.get("/", (req, res) => {
-    res.render("home");
+    
+    let projectInfo = [];
+    let projectImage = "";
+    
+    projectJSON.projects.forEach(project => {
+        
+        if (project["images"].length === 0) {
+            projectImage = "portfolio_snapshots-13.png";
+        } else {
+            projectImage = project["images"][0];
+        }
+        
+        projectInfo = projectInfo.concat({
+            title : project["title"],
+            image : projectImage
+        });
+    });
+    
+    console.log(projectInfo);
+    
+    res.render("home", {
+        gridInfo : projectInfo
+    });
+
 });
 
 module.exports = router;
