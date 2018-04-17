bfc4b9da07413509125761988fed27a9ffc93198
Erin Thigpen <35975360+thigabiga@users.noreply.github.com>
2018-04-16 22:01:12 -0500

    nav stuff

--- public/scripts/nav.js
+++ public/scripts/nav.js
@@ -7,3 +7,12 @@
         i++;
     };
 })();
+
+
+`(function changeNavLinks () {
+    var navLinks = document.getElementsByClassName("nav-hashtag");
+    navLinks[0].href = "/#about";
+    navLinks[1].href = "/#work";
+    navLinks[2].href = "/#design";
+    navLinks[3].href = "/#contact";
+})();`
