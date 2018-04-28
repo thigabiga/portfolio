e1d17ddb4d357aff3463f4aafa5d808475322587
Erin Thigpen <35975360+thigabiga@users.noreply.github.com>
2018-04-28 14:38:53 -0500

    deleted extra nav files

--- public/scripts/reverse-nav.js
+++
@@ -1,9 +0,0 @@
-// reverses nav bar changes when leaving projects page
-
-function reverseNavLinks () {
-    var navLinks = document.getElementsByClassName("nav-hashtag");
-    navLinks[0].href = "#about";
-    navLinks[1].href = "#work";
-    navLinks[2].href = "#design";
-    navLinks[3].href = "#contact";
-};
